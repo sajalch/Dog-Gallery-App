97e1f66a825343048269906b2bebf0aeb7cae08c
sajalch <sajalchakraborty7@gmail.com>
2021-03-21 00:39:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       <br/>
       <Row xs lg="6" sm="12" className="justify-content-md-center">
          {Object.entries(breeds)
-         .filter((breed,i)=>breed[0].includes(searchFilter))
+         .filter((breed,i)=>breed[0].includes(searchFilter.toLocaleLowerCase()))
          .map((breed, i) =><BreedCard key={breed} breed={breed[0]} subBreed={breed[1]}/>)}
       </Row>
     </Container>
